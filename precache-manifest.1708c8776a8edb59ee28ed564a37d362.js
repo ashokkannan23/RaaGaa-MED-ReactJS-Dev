f2d3a1774652c6b9b672f764146ce8e7995bfc02
ashokkannan23 <32809419+ashokkannan23@users.noreply.github.com>
2020-08-19 20:48:29 +0530

    Updates

---
+++ precache-manifest.1708c8776a8edb59ee28ed564a37d362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9dc08a4219fa1c2fc9d2f860fd2b94a",
+    "url": "/RaaGaa-MED-ReactJS-Dev/index.html"
+  },
+  {
+    "revision": "14620af43674044acf9b",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/css/2.1b4266c8.chunk.css"
+  },
+  {
+    "revision": "5bc80b2905602f7424ae",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/css/main.12a06d5a.chunk.css"
+  },
+  {
+    "revision": "14620af43674044acf9b",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/2.18aca7fd.chunk.js"
+  },
+  {
+    "revision": "10300dfa896f6ed2c6255ffefdb0373b",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/2.18aca7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc80b2905602f7424ae",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/main.e8f323c0.chunk.js"
+  },
+  {
+    "revision": "ffac83e354aae9aa96e3",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/runtime-main.867f436d.js"
+  }
+]);

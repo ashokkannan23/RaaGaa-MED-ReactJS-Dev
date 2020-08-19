f6241513515709710ba82e114770ad8cf4d28cab
ashokkannan23 <32809419+ashokkannan23@users.noreply.github.com>
2020-08-19 21:00:56 +0530

    logo updated

--- src/Components/SocialLogoWeb.js
+++ src/Components/SocialLogoWeb.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CSS/SocialLogoWeb.css';
+import RaaGaaMedLogo from './images/logo.png';
 import {FiPhoneCall} from "react-icons/fi";
 import {FaWhatsappSquare} from 'react-icons/fa';
 import {MdMail} from "react-icons/md";
@@ -12,7 +13,7 @@ function SocialLogoWeb() {
         <a href="#contact"><FiPhoneCall/></a>
         <a href="#home"><FaWhatsappSquare/></a>
         <a href="#news"><MdMail/></a>
-    </div> < div className = "logo" > <img onClick= {() => openWebiste()} src="/images/logo.png" alt="logo"/> < p onClick = {
+    </div> < div className = "logo" > <img onClick= {() => openWebiste()} src={RaaGaaMedLogo} alt="logo"/> < p onClick = {
         () => openWebiste()
     } > www.raagaamed.com < /p>
      </div > </>)

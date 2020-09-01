aebec827014b67da1263c552253ac921dfc7f01c
ashokkannan23 <32809419+ashokkannan23@users.noreply.github.com>
2020-09-01 23:28:38 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RaaGaa-MED-ReactJS-Dev/precache-manifest.1054eb3f05d27a9575ac1d978edadfe8.js"
+  "/RaaGaa-MED-ReactJS-Dev/precache-manifest.2e970553d03bedaf4bbe95493ec2dc20.js"
 );
 
 self.addEventListener('message', (event) => {

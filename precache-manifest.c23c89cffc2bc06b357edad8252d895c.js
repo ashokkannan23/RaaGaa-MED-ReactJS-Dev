6f69aee1dcd549f44b000cb7ceb65edd663965b0
ashokkannan23 <32809419+ashokkannan23@users.noreply.github.com>
2020-09-02 23:19:04 +0530

    Updates

---
+++ precache-manifest.c23c89cffc2bc06b357edad8252d895c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9890b6860e3748f6e985ed1a6b193614",
+    "url": "/RaaGaa-MED-ReactJS-Dev/index.html"
+  },
+  {
+    "revision": "467017d48ec601ff1ed2",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/css/2.07451e39.chunk.css"
+  },
+  {
+    "revision": "c1dc18cdaf24359d066c",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/css/main.1fc8ab40.chunk.css"
+  },
+  {
+    "revision": "467017d48ec601ff1ed2",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/2.c52c21c9.chunk.js"
+  },
+  {
+    "revision": "f96f32cfb525b6df1dff377f5e68b79c",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/2.c52c21c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1dc18cdaf24359d066c",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/main.9379deec.chunk.js"
+  },
+  {
+    "revision": "ffac83e354aae9aa96e3",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/js/runtime-main.867f436d.js"
+  },
+  {
+    "revision": "f14a18aab4077edd17ad6cc7f8b23f49",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/media/appointment.f14a18aa.png"
+  },
+  {
+    "revision": "7ab58fe00cca65a205b8ada5d725f2af",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/media/logo.7ab58fe0.png"
+  },
+  {
+    "revision": "5384510dc3d8234d6b49cb10c282e6e2",
+    "url": "/RaaGaa-MED-ReactJS-Dev/static/media/registration_form.5384510d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RaaGaa-MED-ReactJS-Dev/precache-manifest.2e970553d03bedaf4bbe95493ec2dc20.js"
+  "/RaaGaa-MED-ReactJS-Dev/precache-manifest.c23c89cffc2bc06b357edad8252d895c.js"
 );
 
 self.addEventListener('message', (event) => {
